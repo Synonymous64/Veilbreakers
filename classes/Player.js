1b9106e3adadd82cbd36c52866d23c82d455544c
Synonymous64 <prajwalurkude007@gmail.com>
2024-11-12 18:43:15 +0530

    sprite animation and player UI added

--- classes/Player.js
+++ classes/Player.js
@@ -12,16 +12,46 @@ class Player {
       x: this.x + this.width / 2,
       y: this.y + this.height / 2,
     }
+    this.loaded = false;
+    this.image = new Image();
+    this.image.onload = () => {
+      this.loaded = true;
+    }
+    this.image.src = '../images/player.png'
+    this.currentFrame = 0
+    this.elapsedTime = 0;
+    this.sprites = {
+      walkdown: {
+        x: 0, y: 0, width: 16, height: 16
+      },
+    }
+    this.currentSprite = this.sprites.walkdown
   }
 
   draw(c) {
+    if (!this.loaded) return
+
     // Red square debug code
     c.fillStyle = 'rgba(0, 0, 255, 0.5)'
     c.fillRect(this.x, this.y, this.width, this.height)
+
+    const cropbox = {
+      x: 0, y: 0, width: 16, height: 16
+    }
+
+    c.drawImage(this.image, cropbox.x, cropbox.height * this.currentFrame, cropbox.width, cropbox.height, this.x, this.y, this.width, this.height)
   }
 
   update(deltaTime, collisionBlocks) {
     if (!deltaTime) return
+
+    this.elapsedTime += deltaTime
+
+    const intervalToGoToNextFrame = 0.15;
+    if (this.elapsedTime > intervalToGoToNextFrame) {
+      this.currentFrame = (this.currentFrame + 1) % 4
+      this.elapsedTime -= 0.1
+    }
 
     // Update horizontal position and check collisions
     this.updateHorizontalPosition(deltaTime)
